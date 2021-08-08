f12068a3b1caaaf7f03e1ff376417770cbf44e00
evaristocuesta <evaristocg@gmail.com>
2021-08-08 11:31:38 +0200

    #19: twisted and straight pages added

---
+++ docs/js/twisted-particles.js
@@ -0,0 +1,9 @@
+const imagejh = document.getElementById('image-jh');
+
+pict2pix.animate({
+    image: imagejh,
+    numberOfParticles: 800,
+    horizontalSpeed: -1,
+    verticalSpeed: -1,
+    particleType: 'twisted-particle'
+});

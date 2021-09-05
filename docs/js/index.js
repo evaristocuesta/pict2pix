b96450fa14a1a8fa1ccb5ffe49b90420244f3815
evaristocuesta <evaristocg@gmail.com>
2021-09-05 18:35:48 +0200

    #51: 404 page added

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,27 +3,33 @@ const imagejh2 = document.getElementById('image-jh2');
 const imagejh3 = document.getElementById('image-jh3');
 
 window.onload = function initialize() {
-    pict2pix.animate({
-        image: imagejh,
-        numberOfParticles: 800,
-        horizontalSpeed: -1,
-        verticalSpeed: -1,
-        particleType: 'twisted-particle'
+    if (imagejh) {
+        pict2pix.animate({
+            image: imagejh,
+            numberOfParticles: 800,
+            horizontalSpeed: -1,
+            verticalSpeed: -1,
+            particleType: 'twisted-particle'
+        });
+    }
+    if (imagejh2) {
+        pict2pix.animate({
+            image: imagejh2,
+            particleType: 'led-matrix',
+            type: 'random',
+            transitionTime: 2000,
+            idleTime: 5000,
+            ledSize: 4
+        });
+    }
+    if (imagejh3) {
+        pict2pix.animate({
+            image: imagejh3,
+            particleType: 'halftone', 
+            transitionTime: 10000,
+            idleTime: 4000,
+            color: 'rgb(0, 60, 90)', 
+            separation: 8
     });
-    pict2pix.animate({
-        image: imagejh2,
-        particleType: 'led-matrix',
-        type: 'random',
-        transitionTime: 2000,
-        idleTime: 5000,
-        ledSize: 4
-    });
-    pict2pix.animate({
-        image: imagejh3,
-        particleType: 'halftone', 
-        transitionTime: 10000,
-        idleTime: 4000,
-        color: 'rgb(0, 60, 90)', 
-        separation: 8
-  });
+    }
 }
